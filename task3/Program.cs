f22a52b30a7048cf277947a2e0dd768ac49aff3f
Julia <log6117@yandex.ru>
2023-06-21 17:14:33 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+void CubeFrom1toN (int numberN)
+{
+    int current = 1;
+    while (current <= numberN)
+    {
+        double result = Math.Pow(current, 3);
+        Console.Write($"{result:f0}, ");
+        current++;
+    }
+    Console.WriteLine("\b\b.");
+}
+
+Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubeFrom1toN(number);
+
+
+
+// программа для положительных чисел больше 0
